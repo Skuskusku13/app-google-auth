58a182a42c698c1908b1ce812249af34577726ba
Skuskusku13 <danlevy149@gmail.com>
2026-02-10 14:50:03 +0100

    hotfix : fix error phpstan feat : add google authenticator + styles

---
+++ src/Controller/GoogleController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GoogleController extends AbstractController
+{
+    #[Route('/connect/google', name: 'connect_google')]
+    public function connectAction(ClientRegistry $clientRegistry): Response
+    {
+        return $clientRegistry
+            ->getClient('google')
+            ->redirect([
+                'email', 'profile'
+            ]);
+    }
+
+    #[Route('/connect/google/check', name: 'connect_google_check')]
+    public function connectCheckAction(Request $request): Response
+    {
+        // Cette route sera gérée par l'authenticator
+        return new Response('Should not reach here');
+    }
+
+    #[Route('/connect/google/success', name: 'connect_google_success')]
+    public function connectSuccessAction(): Response
+    {
+        return new Response(<<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Connexion réussie</title>
+</head>
+<body>
+    <script>
+        // Si la fenêtre a un "parent" (opener), on le redirige vers le dashboard et on ferme celle-ci
+        if (window.opener) {
+            window.opener.location.href = '/dashboard';
+            window.close();
+        } else {
+            // Fallback si pas de popup (accès direct)
+            window.location.href = '/dashboard';
+        }
+    </script>
+    <p>Connexion réussie. Redirection...</p>
+</body>
+</html>
+HTML
+        );
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+        // Symfony gère automatiquement la déconnexion
+        throw new \LogicException('This method can be blank');
+    }
+}
---
+++ src/Security/GoogleAuthenticator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+
+class GoogleAuthenticator extends OAuth2Authenticator
+{
+    public function __construct(
+        private ClientRegistry $clientRegistry,
+        private EntityManagerInterface $entityManager,
+        private RouterInterface $router
+    ) {}
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->attributes->get('_route') === 'connect_google_check';
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $client = $this->clientRegistry->getClient('google');
+        $accessToken = $this->fetchAccessToken($client);
+
+        return new SelfValidatingPassport(
+            new UserBadge($accessToken->getToken(), function() use ($accessToken, $client) {
+                $googleUser = $client->fetchUserFromToken($accessToken);
+
+                $email = $googleUser->getEmail();
+                $googleId = $googleUser->getId();
+
+                // Chercher l'utilisateur existant
+                $user = $this->entityManager->getRepository(User::class)
+                    ->findOneBy(['googleId' => $googleId]);
+
+                if (!$user) {
+                    // Créer un nouveau compte
+                    $user = new User();
+                    $user->setEmail($email);
+                    $user->setGoogleId($googleId);
+                    $user->setName($googleUser->getName());
+                    $user->setAvatar($googleUser->getAvatar());
+                    $user->setRoles(['ROLE_USER']);
+
+                    $this->entityManager->persist($user);
+                    $this->entityManager->flush();
+                }
+
+                return $user;
+            })
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // Redirigez vers la page de fermeture de popup
+        return new RedirectResponse($this->router->generate('connect_google_success'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new RedirectResponse($this->router->generate('app_login'));
+    }
+}
